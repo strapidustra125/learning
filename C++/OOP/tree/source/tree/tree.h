c257c145d96fdd9d0a0d5a649d0bf9ec61b4b9aa
strapidustra <strapidustra125@gmail.com>
2022-04-26 18:06:39 +0400

    tree: Структура класса Tree.

--- C++/OOP/tree/source/tree/tree.h
+++ C++/OOP/tree/source/tree/tree.h
@@ -1,21 +1,91 @@
-#ifndef _CUSTOM_STRINGS_LIB__
-#define _CUSTOM_STRINGS_LIB__
+#ifndef _TREE_LIB__
+#define _TREE_LIB__
 
 #include <iostream>
+#include <vector>
+#include "customStrings.h"
 
-
+template<typename T>
 class Tree
 {
 private:
-    /* data */
-public:
-    Tree(/* args */);
-    ~Tree();
-
-
     
 
+
+public:
+
+    Tree();
+
+    Tree(Type data)
+    {
+        
+    }
+
+    ~Tree();
+
+    void print();
+
+
+
+
+
+
+    // Индекс вершины
+    static unsigned int _index = 0;
+
+    // Имя, по которому искать элемент
+    String _itemName;
+
+    // Полезные данные любого типа
+    T _data;
+
+    // Указатель на родителя
+    Tree * _pParent;
+
+    // Массив адресов детей
+    std::vector<Tree> * _children;
+
+
+
+
+    // Функции
+
+    // Добавить ребенка
+
+    // Удалить поддерево
+
+    // Обход дерева
+
+
+
+
+
+    // Перегрузить оператор обращения по индексу
+
+    // Оператор обращения по индексу
+    const T& operator [] (String name)
+    {
+
+    }
+
 };
+
+
+
+
+
+
+
+template<typename T>
+void Tree<T>::print()
+{
+
+}
+
+
+
+
+
 
 
 
