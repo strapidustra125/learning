8820ff591688b16b38d29037bcbeda3ebf7e20e2
strapidustra <strapidustra125@gmail.com>
2022-12-14 15:35:05 +0400

    Спринт 2: исправил замечания

--- courses/yandex_dsa/sprint_2/task1.cpp
+++ courses/yandex_dsa/sprint_2/task1.cpp
@@ -1,47 +1,57 @@
-/***********************************************************************************************
- * 
- * Успешная попытка: 79330647
+/**************************************************************************
+ *
+ * Успешная попытка: 79442285
  * 
  * -- ПРИНЦИП РАБОТЫ --
  * 
- * Реализовал Дек на кольцевом буффере: 2 указателя (на первый и на последний элементы), размер
- * и сам динамический массив.
+ * Реализовал Дек на кольцевом буффере: 2 указателя (на первый и на 
+ * последний элементы), размер и сам динамический массив.
  * 
- * Методы добавления push_back и push_front сначала проверяют размер: если об равен 
- * максимальному размеру массива, то добавлять некуда, возврат false. Далее рассматривается 
- * corner case, когда размер = 0 => не надо смещать указатели, а просто записать элемент в 
- * текущую позицию указателя. А если размер не нулевой, то указатель смещается.
- * Проверок пересечения указателей не делаю, потому что достаточно проверки размера Дека.
+ * Методы добавления push_back и push_front сначала проверяют размер: если 
+ * об равен максимальному размеру массива, то добавлять некуда, возврат 
+ * false. Далее рассматривается * corner case, когда размер = 0 => не надо 
+ * смещать указатели, а просто записать элемент в текущую позицию 
+ * указателя. А если размер не нулевой, то указатель смещается. Проверок 
+ * пересечения указателей не делаю, потому что достаточно проверки размера 
+ * Дека.
  * 
- * Методы удаления pop_back и pop_front так же сначала проверяют размер, но уже на 0. Далее 
- * corner case на size = 1 => смещать указатель не надо, т.к. это последний элемент.
+ * Методы удаления pop_back и pop_front так же сначала проверяют размер, но 
+ * уже на 0. Далее corner case на size = 1 => смещать указатель не надо, 
+ * т.к. это последний элемент.
  * 
- * Все функции чтения входных данных реализовал соответствующими функциями. Даже первые 2 числа  
- * читаю через std::getline, т.к. если просто прочитать строковую переменную через std::cin, 
- * потом первый std::getline прочитает пустую строку. 
+ * Все функции чтения входных данных реализовал соответствующими функциями. 
+ * Даже первые 2 числа читаю через std::getline, т.к. если просто прочитать 
+ * строковую переменную через std::cin, потом первый std::getline прочитает 
+ * пустую строку. 
  * 
- * Обработка команд просто через if, не стал вводить красивые emum'ы, потому что программа 
- * маленькая и понятная. Везде использую this для полей класса внутри класса, чтобы визуально
- * поля класса отличались от обычных переменных.
+ * Обработка команд просто через if, не стал вводить красивые emum'ы, 
+ * потому что программа маленькая и понятная. Везде использую this для 
+ * полей класса внутри класса, чтобы визуально поля класса отличались от 
+ * обычных переменных.
  * 
  * -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
  * 
- * Все операции контролирует размер массива, который может изменииться только с шагом 1, поэтому 
- * указатели не обгонят друг друга. Остальные частные случаи обработаны: добавление в пустой 
- * массив и взятие из массива с размером 1 - в этих случаях не надо двигать указатели, только 
- * менять ращмер.
+ * Все операции контролирует размер массива, который может изменииться 
+ * только с шагом 1, поэтому указатели не обгонят друг друга. Остальные 
+ * частные случаи обработаны: добавление в пустой массив и взятие из 
+ * массива с размером 1 - в этих случаях не надо двигать указатели, только 
+ * менять размер.
  * 
  * -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
  * 
- * Все 4 операции выполняются за О(1), т.к. нигде нет циклов, кроме чтения входных данных (но от
- * него избавиться не получится).
+ * Все 4 операции выполняются за О(1), т.к. нигде нет циклов. Общая 
+ * сложность - O(n), где n - количество команд во входных данных. От 
+ * максимального размера Дека (m) временная сложность не зависит.
  * 
  * -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
  * 
- * Все переменные, кроме размера массива _deque, занимают О(1) памяти. А массив - О(n), поэтому 
- * общие затраты памяти - О(n), где n - размер кольцевого буффера.
+ * Все переменные, кроме размера массива _deque, занимают О(1) памяти. А 
+ * массив - О(m), поэтому общие затраты памяти - О(m), где m - максимальный 
+ * размер Дека. Количество команд (n) не влияет на пространственную 
+ * сложность, т.к. независимо от числа команд, каждую итерацию цикла все
+ * переменные, относящиеся к обработке текущей команды, очищаются. 
  * 
- **********************************************************************************************/
+ *************************************************************************/
 
 #include <iostream>
 #include <set>
@@ -49,17 +59,18 @@
 #include <algorithm>
 #include <fstream>
 #include <string>
+#include <optional>
 
 #ifdef MY_IDE
-#define out fout
-#define in fin
+    #define out fout
+    #define in fin
 #else
-#define out std::cout
-#define in std::cin
+    #define out std::cout
+    #define in std::cin
 #endif
 
-std::ifstream fin("in.txt");
-std::ofstream fout("out.txt");
+std::ifstream fin("../in.txt");
+std::ofstream fout("../out.txt");
 
 #define MAX_DEQUE_SIZE 50000
 
@@ -67,93 +78,81 @@ class Deque
 {
 private:
 
-	int* _deque;
-	int _size;
-	int _maxSize;
+    int* _deque;
+    int _size;
+    int _maxSize;
 
-	int _head;
-	int _tail;
+    int _head;
+    int _tail;
 
 public:
 
-	Deque(const int & dequeMaxSize=MAX_DEQUE_SIZE)
-	{
-		this->_maxSize = dequeMaxSize;
-		this->_size = 0;
-		this->_deque = new int[dequeMaxSize];
+    Deque(const int & dequeMaxSize=MAX_DEQUE_SIZE)
+    {
+        this->_maxSize = dequeMaxSize;
+        this->_size = 0;
+        this->_deque = new int[dequeMaxSize];
 
-		this->_head = 0;
-		this->_tail = 0;
-	}
+        this->_head = 0;
+        this->_tail = 0;
+    }
 
-	bool push_back(const int& value);
-	bool push_front(const int& value);
-	bool pop_back(int& value);
-	bool pop_front(int& value);
+    bool push_back(const int& value);
+    bool push_front(const int& value);
+    std::optional<int> pop_back();
+    std::optional<int> pop_front();
 
-	int size() const;
+    int size() const;
 };
 
 
 bool Deque::push_back(const int& value)
 {
-	if (this->size() == this->_maxSize) return false;
+    if (this->size() == this->_maxSize) return false;
 
-	if(this->size())
-		if (this->_tail == 0) this->_tail = this->_maxSize - 1;
-		else this->_tail--;
-	
-	this->_deque[this->_tail] = value;
+    this->_tail = (this->_tail + this->_maxSize - 1) % this->_maxSize; 
+    this->_deque[this->_tail] = value;
 
-	this->_size++;
-	return true;
+    this->_size++;
+    return true;
 }
 
 bool Deque::push_front(const int& value)
 {
-	if (this->size() == this->_maxSize) return false;
+    if(this->size() == this->_maxSize) return false;
 
-	if (this->size())
-		if (this->_head == this->_maxSize - 1) this->_head = 0;
-		else this->_head++;
+    this->_deque[this->_head] = value;
+    this->_head = (this->_head + 1) % this->_maxSize; 
 
-	this->_deque[this->_head] = value;
-
-	this->_size++;
-	return true;
+    this->_size++;
+    return true;
 }
 
-bool Deque::pop_back(int& value)
+std::optional<int> Deque::pop_back()
 {
-	if (!this->size()) return false;
+    if(!this->size()) return std::nullopt;
 
-	value = this->_deque[this->_tail];
+    int value = this->_deque[this->_tail];
+    this->_tail = (this->_tail + 1) % this->_maxSize;
 
-	if(this->size() != 1)
-		if (this->_tail == this->_maxSize - 1) this->_tail = 0;
-		else this->_tail++;
-
-	this->_size--;
-	return true;
+    this->_size--;
+    return value;
 }
 
-bool Deque::pop_front(int& value)
+std::optional<int> Deque::pop_front()
 {
-	if (!this->size()) return false;
+    if(!this->size()) return std::nullopt;
 
-	value = this->_deque[this->_head];
+    this->_head = (this->_head + this->_maxSize - 1) % this->_maxSize;
+    int value = this->_deque[this->_head];
 
-	if (this->size() != 1)
-		if (this->_head == 0) this->_head = this->_maxSize - 1;
-		else this->_head--;
-
-	this->_size--;
-	return true;
+    this->_size--;
+    return value;
 }
 
 int Deque::size() const
 {
-	return this->_size;
+    return this->_size;
 }
 
 
@@ -161,77 +160,77 @@ int Deque::size() const
 
 int readCommandsNumber()
 {
-	std::string line;
-	std::getline(in, line);
-	return std::stoi(line);
+    std::string line;
+    std::getline(in, line);
+    return std::stoi(line);
 }
 
 int readMaxDequeSize()
 {
-	std::string line;
-	std::getline(in, line);
-	return std::stoi(line);
+    std::string line;
+    std::getline(in, line);
+    return std::stoi(line);
 }
 
 std::string readCommand(int& param)
 {
-	std::string commandLine;
-	std::getline(in, commandLine);
+    std::string commandLine;
+    std::getline(in, commandLine);
 
-	if (commandLine.find(' ') == std::string::npos) return commandLine;
+    if (commandLine.find(' ') == std::string::npos) return commandLine;
 
-	std::string command = "";
-	std::string number = "";
-	bool secondWord = false;
+    std::string command = "";
+    std::string number = "";
+    bool secondWord = false;
 
-	for (int j = 0; j < commandLine.size(); j++)
-	{
-		if (secondWord) number += commandLine[j];
-		else
-		{
-			if (commandLine[j] == ' ') secondWord = true;
-			else command += commandLine[j];
-		}
-	}
+    for (int j = 0; j < commandLine.size(); j++)
+    {
+        if (secondWord) number += commandLine[j];
+        else
+        {
+            if (commandLine[j] == ' ') secondWord = true;
+            else command += commandLine[j];
+        }
+    }
 
-	param = std::stoi(number);
-	return command;
+    param = std::stoi(number);
+    return command;
 }
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-	int commandsNumber = readCommandsNumber();
-	Deque deque(readMaxDequeSize());
+    int commandsNumber = readCommandsNumber();
+    Deque deque(readMaxDequeSize());
 
-	for (int i = 0; i < commandsNumber; i++)
-	{
-		int param;
-		std::string command = readCommand(param);
+    for (int i = 0; i < commandsNumber; i++)
+    {
+        int param;
+        std::string command = readCommand(param);
 
-		if(command == "push_front")
-		{
-			if (!deque.push_front(param)) out << "error" << std::endl;
-		}
-		else if (command == "push_back")
-		{
-			if (!deque.push_back(param)) out << "error" << std::endl;
-		}
-		else if (command == "pop_front")
-		{
-			int value;
-			if (!deque.pop_front(value)) out << "error" << std::endl;
-			else out << value << std::endl;
-		}
-		else if (command == "pop_back")
-		{
-			int value;
-			if (!deque.pop_back(value)) out << "error" << std::endl;
-			else out << value << std::endl;
-		}
-	}
+        if(command == "push_front")
+        {
+            if (!deque.push_front(param)) out << "error" << std::endl;
+        }
+        else if (command == "push_back")
+        {
+            if (!deque.push_back(param)) out << "error" << std::endl;
+        }
+        else if (command == "pop_front")
+        {
+            std::optional<int> value = deque.pop_front();
+            if (value == std::nullopt) out << "error" << std::endl;
+            else out << *value << std::endl;
+        }
+        else if (command == "pop_back")
+        {
+            std::optional<int> value = deque.pop_back();
+            if (value == std::nullopt) out << "error" << std::endl;
+            else out << *value << std::endl;
+        }
+    }
 
-	return 0;
+    return 0;
 }
--- courses/yandex_dsa/sprint_2/task2.cpp
+++ courses/yandex_dsa/sprint_2/task2.cpp
@@ -1,34 +1,37 @@
-/***********************************************************************************************
+/**************************************************************************
  * 
  * Успешная попытка: 79330678
  * 
  * -- ПРИНЦИП РАБОТЫ --
  * 
- * Вычитал в канале practice, что можно использовать встроенный стек, чем и воспользовался.
- * Но в промежуточных задачах уже реализовывал стек, поэтому не сложно добавить самодельный в
- * случае необходимости.
- * Принцип работы: читаю символ, getOperator говорит мне, что это за оператор: если none - 
- * кладу элемент в стек и перехожу к следующему элементу, если одна из операций - достаю из 
- * стека 2 элемента и совершаю над ними операцию. Результат кладу в стек. Когда все входные 
- * данные закончатся, то, в случае правильности входных данных (верю наслово), в стеке останется
- * результат. Его вывожу.
+ * Вычитал в канале practice, что можно использовать встроенный стек, чем и
+ * воспользовался. Но в промежуточных задачах уже реализовывал стек, 
+ * поэтому не сложно добавить самодельный в случае необходимости.
+ * Принцип работы: читаю символ, getOperator говорит мне, что это за 
+ * оператор: если none - кладу элемент в стек и перехожу к следующему 
+ * элементу, если одна из операций - достаю из стека 2 элемента и совершаю 
+ * над ними операцию. Результат кладу в стек. Когда все входные данные 
+ * закончатся, то, в случае правильности входных данных (верю наслово), в 
+ * стеке останется результат. Его вывожу.
  * 
  * -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
  * 
- * Результат текущей операции всегда помещается в стек и становится одним из операднов для 
- * следующей операции.
+ * Результат текущей операции всегда помещается в стек и становится одним 
+ * из операднов для следующей операции.
  * 
  * -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
  * 
- * Все используемые операции реализованные в std::stack выполняются за O(1). Самая затратрая 
- * операция - чтение входных данных за О(n) => общая сложность O(n).
+ * Все используемые операции реализованные в std::stack выполняются за 
+ * O(1). Самая затратрая операция - чтение входных данных за О(n) => общая 
+ * сложность O(n).
  * 
  * -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
  * 
- * Все переменных, кроме самого стека - О(1). У стека максимальный размер в случае, если сначала 
- * во входных данных идут все числа, а потом все операторы, т.е. (n+1)/2 - оно же О(n).
+ * Все переменных, кроме самого стека - О(1). У стека максимальный размер в
+ * случае, если сначала во входных данных идут все числа, а потом все 
+ * операторы, т.е. (n+1)/2 - оно же О(n).
  * 
- **********************************************************************************************/
+ *************************************************************************/
 
 #include <iostream>
 #include <fstream>
@@ -51,68 +54,69 @@ std::ofstream fout("out.txt");
 
 enum Operator
 {
-	none,
-	addition,
-	subtraction,
-	multiplication,
-	division
+    none,
+    addition,
+    subtraction,
+    multiplication,
+    division
 };
 
 Operator getOperator(std::string symbol)
 {
-	if (symbol == "+") return Operator::addition;
-	if (symbol == "-") return Operator::subtraction;
-	if (symbol == "*") return Operator::multiplication;
-	if (symbol == "/") return Operator::division;
-	return Operator::none;
+    if (symbol == "+") return Operator::addition;
+    if (symbol == "-") return Operator::subtraction;
+    if (symbol == "*") return Operator::multiplication;
+    if (symbol == "/") return Operator::division;
+    return Operator::none;
 }
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-	std::stack<int> stack;
+    std::stack<int> stack;
 
-	std::string symbol;
-	while (in >> symbol)
-	{
-		Operator oper = getOperator(symbol);
+    std::string symbol;
+    while (in >> symbol)
+    {
+        Operator oper = getOperator(symbol);
 
-		if (oper == Operator::none)
-		{
-			stack.push(std::stoi(symbol));
-			continue;
-		}
+        if (oper == Operator::none)
+        {
+            stack.push(std::stoi(symbol));
+            continue;
+        }
 
-		int result;
-		int operand_2 = stack.top();
-		stack.pop();
-		int operand_1 = stack.top();
-		stack.pop();
+        int result;
+        int operand_2 = stack.top();
+        stack.pop();
+        int operand_1 = stack.top();
+        stack.pop();
 
-		switch (oper)
-		{
-		case addition:
-			result = operand_1 + operand_2;
-			break;
-		case subtraction:
-			result = operand_1 - operand_2;
-			break;
-		case multiplication:
-			result = operand_1 * operand_2;
-			break;
-		case division:
-			result = operand_1 / operand_2;
-			if(operand_1 % operand_2 != 0)
-				if (operand_1 < 0 && operand_2 > 0 || operand_1 > 0 && operand_2 < 0) result--;
-			break;
-		}
+        switch (oper)
+        {
+        case addition:
+            result = operand_1 + operand_2;
+            break;
+        case subtraction:
+            result = operand_1 - operand_2;
+            break;
+        case multiplication:
+            result = operand_1 * operand_2;
+            break;
+        case division:
+            result = operand_1 / operand_2;
+            if(operand_1 % operand_2 != 0)
+                if (operand_1 < 0 && operand_2 > 0 || 
+					operand_1 > 0 && operand_2 < 0) result--;
+            break;
+        }
 
-		stack.push(result);
-	}
+        stack.push(result);
+    }
 
-	out << stack.top();
+    out << stack.top();
 
-	return 0;
+    return 0;
 }
