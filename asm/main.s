7ed7918afefcc0b0fbd8b017bd017e8b5b4f5809
Nikolay Novikov <n.a.novikov@1440.space>
2025-09-10 13:19:05 +0300

    Добавил скрипт для генерации чистого ассемблера.

--- asm/main.s
+++ asm/main.s
@@ -1,8 +1,3 @@
-	.file	"main.cpp"
-	.intel_syntax noprefix
-	.text
-	.globl	_Z6squarei
-	.type	_Z6squarei, @function
 _Z6squarei:
 	push	rbp
 	mov	rbp, rsp
@@ -11,9 +6,6 @@ _Z6squarei:
 	imul	eax, eax
 	pop	rbp
 	ret
-	.size	_Z6squarei, .-_Z6squarei
-	.globl	main
-	.type	main, @function
 main:
 	push	rbp
 	mov	rbp, rsp
@@ -24,22 +16,3 @@ main:
 	movzx	eax, al
 	pop	rbp
 	ret
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04.2) 11.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
